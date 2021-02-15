c6b8ea304d295267ff462bd169fcb66335683e3d
chrisb3546 <chrisbradycode@gmail.com>
2021-02-15 08:59:13 -0500

    add edit form for blogs,add users index link to users show

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,15 +1,16 @@
 class BlogsController < ApplicationController
     before_action :find_blog, only: [:show, :edit,:update,:destroy]
+    before_action :redirect_if_not_logged_in, only: [:new, :edit]
 
     def show
     end
 
     def new
-        @blog = Blog.new
+        @blog = current_user.blogs.build
     end
 
     def create
-        @blog = Blog.new(blog_params)
+        @blog = current_user.blogs.build(blog_params)
         if @blog.save
             redirect_to blog_path(@blog)
         else
@@ -37,7 +38,7 @@ class BlogsController < ApplicationController
     private
 
     def blog_params
-        params.require(:blog).permit(:title, :description, :user_id)
+        params.require(:blog).permit(:title, :content, :user_id)
     end
 
     def find_blog
