36494d8a44b59a50076588ace4429fb0199fdbd4
chrisb3546 <chrisbradycode@gmail.com>
2021-02-21 16:19:49 -0500

    modify user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :blogs, dependent: :destroy
-    validates :username, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
 
 end
