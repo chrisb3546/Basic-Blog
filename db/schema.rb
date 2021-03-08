a456165a83795a96f39d477b09ce94481c5243c1
chrisb3546 <chrisbradycode@gmail.com>
2021-03-08 14:36:41 -0500

    add admin logic to views for blogs and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_141936) do
+ActiveRecord::Schema.define(version: 2021_03_07_142202) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_03_07_141936) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "blogs", "users"
