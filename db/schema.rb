33d7cc584af171843b9a0781148f1a78ff5f285f
chrisb3546 <chrisbradycode@gmail.com>
2021-03-21 16:55:47 -0400

    add and pass first test to category_test.rb, remove auto generated tests from rails g resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_142202) do
+ActiveRecord::Schema.define(version: 2021_03_21_204536) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2021_03_07_142202) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
