6647c09a6a3d87e968bfa798efb276955d235b56
chrisb3546 <chrisbradycode@gmail.com>
2021-03-22 11:27:06 -0400

    add categorycontroller and category controller tests to application

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
   resources :blogs
   resources :users, except: [:new]
+  resources :categories, except: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
